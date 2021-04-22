137e06461b2fd63aca989832b706e5f192845190
NaHyeon <nahyeonee99@gmail.com>
2021-04-22 21:23:13 +0900

    #2.1 useTitle

---
+++ useInput/useTitle.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+export default function App() {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return (
+    <div className="App">
+      <div>Hi</div>
+    </div>
+  );
+}
