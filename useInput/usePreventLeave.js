0ad88530d505899fc250e0c4214e656c5811d29c
NaHyeon <nahyeonee99@gmail.com>
2021-04-23 15:34:58 +0900

    #2.3 usePreventLeave

---
+++ useInput/usePreventLeave.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
+};
+
+export default function App() {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>unprotect</button>
+    </div>
+  );
+}
