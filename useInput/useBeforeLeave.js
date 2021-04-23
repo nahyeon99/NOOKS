2f34e6daa51251a7aa391a11ccac031154bc338a
NaHyeon <nahyeonee99@gmail.com>
2021-04-23 16:00:37 +0900

    #2.4 useBeforeLeave

---
+++ useInput/useBeforeLeave.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useBeforeLeave = (onBefore) => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = (event) => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => document.removeEventListener("mouseleave", handle);
+  }, []);
+};
+
+export default function App() {
+  const dontgo = () => console.log("Please don't go");
+  useBeforeLeave(dontgo);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
